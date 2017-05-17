9ae2edf24d49ba89f0f19afbd5d0e8de2c9a9cf5
Eugene Pimenov <eugene@libc.st>
2017-05-17 23:17:41 +0200

    Add some chat functionality

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,81 @@
+//= require ./cable
+
+var buttonPresses = 0;
+
+var messages = {
+  one: 'Nice one!',
+  five: 'Wow, you are really fidgiting!',
+  ten: "Watch out for the sore thumb!",
+  sevenish: 'Ooh, just the way I like it',
+  thirteenish: 'Yes yes yes, do it again',
+  arm_twister: "Don't twist my arm",
+  round_the_world: 'Round round the world it goes',
+}
+
+var hitZero;
+App.cable.subscriptions.create({ channel: "EventsChannel" }, {
+  received: function(data) {
+    if (data.event_type == 'button') {
+      buttonPresses++;
+    }
+
+    if (data.event_type == 'meter') {
+      if (data.value == 10) {
+        triggerOnce('arm_twister');
+      }
+
+      if (data.value == 0) {
+        hitZero = true;
+      }
+      if (data.value == 10 && hitZero) {
+        trigger('round_the_world');
+        hitZero = false;
+      }
+    }
+
+    calculateMessages()
+  }
+})
+
+var shown = {};
+
+function calculateMessages() {
+  if (buttonPresses > 0) { triggerOnce('one'); }
+  if (buttonPresses > 5) { triggerOnce('five'); }
+  if (buttonPresses > 10) { triggerOnce('ten'); }
+  if (buttonPresses % 7 == 0 && buttonPresses > 0) { trigger('sevenish'); }
+  if (buttonPresses % 13 == 0 && buttonPresses > 0) { trigger('thirteenish'); }
+}
+
+var $masterWrapper;
+
+$(function() {
+  $masterWrapper = $('.speech-wrapper .bubble').remove()
+});
+
+function addMessage(message) {
+  var c = $masterWrapper.clone().hide()
+  c.find('.message').text(message)
+
+  var d = new Date();
+  var hour = "" + d.getHours();
+  if (hour.length == 1) hour = "0" + hour;
+  var mins = "" + d.getMinutes();
+  if (mins.length == 1) mins = "0" + mins;
+  c.find('.timestamp').text(hour + ':' + mins)
+  $('.speech-wrapper').prepend(c);
+  c.slideDown()
+
+  $('.zero-state').hide()
+}
+
+var seen = {};
+function triggerOnce(message) {
+  if (seen[message]) return;
+  seen[message] = true;
+  trigger(message)
+}
+
+function trigger(message) {
+  addMessage(messages[message]);
+}
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ApplicationRecord
 
   after_save :pour_beer, if: -> { event_type == :button }
   after_commit :create_badges
+  after_commit :send_events
 
   scope :button_presses, -> { where("events.event_type = ?", 'button') }
   scope :today, -> { where(created_at: (Time.now.beginning_of_day..Time.now)) }
@@ -12,6 +13,10 @@ class Event < ApplicationRecord
 
   def pour_beer
     ActionCable.server.broadcast 'beer', percentage: 5, pushes: Event.where(event_type: :button, created_at: 5.minutes.ago..Time.now).count
+  end
+
+  def send_events
+    ActionCable.server.broadcast 'events', attributes
   end
 
   def create_badges
